a1fe139be19ac542ee7cfd78b45f364eda22757b
Oles Maiboroda <oles.maiboroda@gmail.com>
2018-10-05 13:46:27 +0200

    Init

---
+++ packages/H1/build.js
@@ -0,0 +1,17 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var H1 = function H1() {
+  return _react.default.createElement("h1", null, "Header");
+};
+
+var _default = H1;
+exports.default = _default;
---
+++ packages/H1/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const H1 = () => <h1>Header</h1>;
+
+export default H1;
